0b8c133166a0db4db72ec90f7bbb96de4d0def04
Sergio Perez <sperez4mba@gmail.com>
2014-07-14 09:31:24 -0500

    ReaderWriter class: provides methods to read/write from/to the socket.

---
+++ src/com/sperez/jhs/ReaderWriter.java
@@ -0,0 +1,59 @@
+package com.sperez.jhs;
+
+import java.io.*;
+import java.io.IOException;
+import java.net.*;
+
+public class ReaderWriter {
+    private InputStreamReader in;
+    private BufferedReader inFromClient;
+    private BufferedOutputStream out;
+    private OutputStreamWriter outToClient;
+
+    public ReaderWriter(SocketInterface connectionSocket) {
+        in = new InputStreamReader(connectionSocket.getInput());
+        inFromClient = new BufferedReader (in);
+
+        out = new BufferedOutputStream (connectionSocket.getOutput());
+        outToClient = new OutputStreamWriter(out);
+//        outToClient = new OutputStreamWriter(new BufferedOutputStream (connectionSocket.getOutput()));
+    }
+
+    public void writeLine(String line) {
+        try{
+            outToClient.write(line);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void writeAll(String response){
+        try{
+            String[] tokenizedResponse = response.split("\r\n");
+            for(int i=0; i < tokenizedResponse.length; i++){
+                outToClient.write(tokenizedResponse[i]);
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void sendAll(){
+        try{
+            outToClient.flush();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public String readLine(){
+        String lineReceived = "";
+        try{
+            lineReceived = inFromClient.readLine();
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        return lineReceived;
+    }
+}
