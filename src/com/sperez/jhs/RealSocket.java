51d22591a6c5de01258f2dfae2417e80f9207a3a
Sergio Perez <sperez4mba@gmail.com>
2014-07-14 09:55:08 -0500

    RealSocket class: wrapper class around the real socket.

---
+++ src/com/sperez/jhs/RealSocket.java
@@ -0,0 +1,42 @@
+package com.sperez.jhs;
+
+import java.io.*;
+import java.net.*;
+import java.io.IOException;
+
+public class RealSocket implements SocketInterface {
+    private Socket connectionSocket;
+
+    public RealSocket(Socket connectionSocket){
+        this.connectionSocket = connectionSocket;
+    }
+
+    public InputStream getInput(){
+        InputStream input = null;
+        try{
+        input = connectionSocket.getInputStream();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        return input;
+    }
+
+    public OutputStream getOutput(){
+        OutputStream output = null;
+        try{
+            output = connectionSocket.getOutputStream();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        return output;
+    }
+
+
+    public void close() {
+        try{
+            connectionSocket.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
