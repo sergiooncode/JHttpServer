e5a0b23046b395cb516d93615cb3bde74e74eb8e
Sergio Perez <sperez4mba@gmail.com>
2014-07-14 16:56:10 -0500

    Refactored to inject dependency of ResponseMaker.

--- src/com/sperez/jhs/Main.java
+++ src/com/sperez/jhs/Main.java
@@ -5,10 +5,13 @@ public class Main {
         ServerSocketInterface listenSocket;
         SimpleServer server;
         ArgumentsParser parser;
+        ResponseMaker maker;
 
         parser = new ArgumentsParser(args);
         listenSocket = new RealServerSocket(parser.getPort());
         server = new SimpleServer(listenSocket);
+        maker = new ResponseMaker();
+        server.setupResponseMaker(maker);
 
         server.run();
     }
