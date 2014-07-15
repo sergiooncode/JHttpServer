33c5ee7573f9b4abd835d7617a356e290ed72f59
Sergio Perez <sperez4mba@gmail.com>
2014-07-15 16:16:40 -0500

    Refactoring.

--- test/com/sperez/jhs/ConnectionHandlerTest.java
+++ test/com/sperez/jhs/ConnectionHandlerTest.java
@@ -2,16 +2,22 @@ package com.sperez.jhs;
 
 import com.sperez.jhs.mocks.*;
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.instanceOf;
 
 public class ConnectionHandlerTest {
+    private ServerSocketInterface listenSocket;
+    private ConnectionHandler handler;
+
+    @Before
+    public void setUp() {
+        listenSocket = new MockServerSocket();
+        handler = new ConnectionHandler(listenSocket);
+    }
 
     @Test
     public void testConnect() throws Exception {
-        ServerSocketInterface listenSocket = new MockServerSocket();
-        ConnectionHandler handler = new ConnectionHandler(listenSocket);
-
         handler.connect();
 
         assertThat(handler.getConnectionSocket(), instanceOf(MockSocket.class));
