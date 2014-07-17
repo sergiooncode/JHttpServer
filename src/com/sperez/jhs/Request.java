37f8621126cece88b69c7563b967ea22b1b3e7c0
Sergio Perez <sperez4mba@gmail.com>
2014-07-17 11:48:16 -0500

    Added a few more test for RequestHandler class and more functionality to Request class.

--- src/com/sperez/jhs/Request.java
+++ src/com/sperez/jhs/Request.java
@@ -1,9 +1,31 @@
 package com.sperez.jhs;
 
 public class Request {
-    private String requestLine;
+    private String requestMethod;
+    private String protocol;
+    private String requestBody;
+    private String requestedResource;
 
-    public Request(String requestLine) {
-        this.requestLine = requestLine;
+    public Request(String requestMethod, String requestedResource, String protocol, String requestBody) {
+        this.requestMethod = requestMethod;
+        this.requestedResource = requestedResource;
+        this.protocol = protocol;
+        this.requestBody = requestBody;
+    }
+
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public String getRequestBody() {
+        return requestBody;
+    }
+
+    public String getRequestedResource() {
+        return requestedResource;
     }
 }
