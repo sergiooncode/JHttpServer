0d58b8cc636791e1798eb3ba20d972d35cc155d9
Sergio Perez <sperez4mba@gmail.com>
2014-07-17 10:08:14 -0500

    Refactored Main class so that it injects ResponseHandler and RequestHandler dependencies.

--- src/com/sperez/jhs/Main.java
+++ src/com/sperez/jhs/Main.java
@@ -5,15 +5,19 @@ public class Main {
         ServerSocketInterface listenSocket;
         SimpleServer server;
         ArgumentsParser parser;
-        ResponseMaker maker;
+        RequestHandler requestHandler;
+        ResponseHandler responseHandler;
         ConnectionHandler connectionHandler;
 
         parser = new ArgumentsParser(args);
         listenSocket = new RealServerSocket(parser.getPort());
         connectionHandler = new ConnectionHandler(listenSocket);
         server = new SimpleServer(connectionHandler);
-        maker = new ResponseMaker();
-        server.setupResponseMaker(maker);
+
+        requestHandler = new RequestHandler();
+        responseHandler = new ResponseHandler();
+        server.setupRequestHandler(requestHandler);
+        server.setupResponseHandler(responseHandler);
 
         server.run();
     }
