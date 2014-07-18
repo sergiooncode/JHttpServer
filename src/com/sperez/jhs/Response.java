9bd479b1a9685d28ffbacde55f1c2b6b12838f8b
Sergio Perez <sperez4mba@gmail.com>
2014-07-18 09:20:13 -0500

    Added some more functionality to Response class.

--- src/com/sperez/jhs/Response.java
+++ src/com/sperez/jhs/Response.java
@@ -1,13 +1,27 @@
 package com.sperez.jhs;
 
+import java.util.ArrayList;
+
 public class Response {
     private String statusLine;
+    private ArrayList<String> responseHeaders;
+    private String responseBody;
 
-    public Response(String statusLine) {
+    public Response(String statusLine, ArrayList<String> responseHeaders, String responseBody) {
         this.statusLine = statusLine;
+        this.responseHeaders = responseHeaders;
+        this.responseBody = responseBody;
     }
 
     public String getStatusLine() {
         return statusLine;
+    }
+
+    public ArrayList<String> getResponseHeaders() {
+        return responseHeaders;
+    }
+
+    public String getResponseBody() {
+        return responseBody;
     }
 }
