c7747818a54dbd07fa1510f45dbd7688ec9e5eac
Sergio Perez <sperez4mba@gmail.com>
2014-07-23 09:06:49 -0500

    Refactored so it creates one thread (one instance of ClientHandler) per connection.

--- src/com/sperez/jhs/SimpleServer.java
+++ src/com/sperez/jhs/SimpleServer.java
@@ -3,51 +3,25 @@ package com.sperez.jhs;
 public class SimpleServer {
     private boolean keepGoing;
     private ConnectionHandler connectionHandler;
-    private ReaderWriter readerWriter;
-    private ResponseHandler responseHandler;
-    private RequestHandler requestHandler;
-    private Request request;
 
     public SimpleServer (ConnectionHandler connectionHandler) {
         this.connectionHandler = connectionHandler;
     }
 
-    private void setupInputOutput(){
-        readerWriter = new ReaderWriter(connectionHandler);
-        requestHandler.setupInputReader(readerWriter);
-        responseHandler.setupOutputWriter(readerWriter);
-    }
-
-    public void setupResponseHandler(ResponseHandler responseHandler) {
-        this.responseHandler = responseHandler;
-    }
-
-    public void setupRequestHandler(RequestHandler requestHandler) {
-        this.requestHandler = requestHandler;
-    }
-
     public void handleClient() {
-        requestHandler.handle();
-        request = requestHandler.getRequestObject();
-        responseHandler.handle(request);
+        new Thread (new ClientHandler(connectionHandler)).start();
     }
 
     private void connect() {
         connectionHandler.connect();
     }
 
-    private void disconnect() {
-        connectionHandler.disconnect();
-    }
-
-    public void run (){
+    public void run() {
         keepGoing = true;
 
         while(keepGoing()){
             connect();
-            setupInputOutput();
             handleClient();
-            disconnect();
         }
     }
 
