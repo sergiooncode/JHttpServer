0f7f5e15ec4118f105df267f0b138c0737cc8a94
Sergio Perez <sperez4mba@gmail.com>
2014-07-23 09:08:12 -0500

    Refactored so that RequestHandler and ResponseHandler instances are created in ClientHandler class.

--- src/com/sperez/jhs/Main.java
+++ src/com/sperez/jhs/Main.java
@@ -5,19 +5,12 @@ public class Main {
         ServerSocketInterface listenSocket;
         SimpleServer server;
         ArgumentsParser parser;
-        RequestHandler requestHandler;
-        ResponseHandler responseHandler;
         ConnectionHandler connectionHandler;
 
         parser = new ArgumentsParser(args);
         listenSocket = new RealServerSocket(parser.getPort());
         connectionHandler = new ConnectionHandler(listenSocket);
         server = new SimpleServer(connectionHandler);
-
-        requestHandler = new RequestHandler();
-        responseHandler = new ResponseHandler();
-        server.setupRequestHandler(requestHandler);
-        server.setupResponseHandler(responseHandler);
 
         server.run();
     }
