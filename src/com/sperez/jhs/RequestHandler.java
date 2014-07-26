74c8f91c73a0644eb4bd1d901c1f9c0949daafdf
Sergio Perez <sperez4mba@gmail.com>
2014-07-26 17:30:04 -0500

    Refactored RequestHandler after creating RequestParser.

--- src/com/sperez/jhs/RequestHandler.java
+++ src/com/sperez/jhs/RequestHandler.java
@@ -3,22 +3,10 @@ package com.sperez.jhs;
 import java.util.ArrayList;
 
 public class RequestHandler {
-    final private String CRLF = "\r\n\r\n";
-    final private String DOUBLE_CRLF = "\r\n\r\n\r\n\r\n";
     private ReaderWriter reader;
     private String rawRequest;
-    private String requestLineAndHeaders;
+    private RequestParser parser;
     private Request requestObject;
-    private String requestLine;
-    private String requestBody;
-    private String requestMethod;
-    private String requestedResource;
-    private String protocol;
-    private ArrayList<String> requestHeaders = new ArrayList<String>();
-
-    void setRawRequest(String rawRequest) {
-        this.rawRequest = rawRequest;
-    }
 
     public void handle() {
         readRequest();
@@ -26,8 +14,9 @@ public class RequestHandler {
     }
 
     public void createRequestObject() {
-        parseRequest();
-        requestObject = new Request(requestMethod, requestedResource, protocol, requestHeaders, requestBody);
+        createParser();
+        parser.parseRequest();
+        requestObject = new Request(method(), resource(), protocol(), headers(), body());
     }
 
     public Request getRequestObject() {
@@ -43,76 +32,31 @@ public class RequestHandler {
         System.out.println("Request: " + rawRequest);
     }
 
-    void parseRequest() {
-        parseRequestLinePlusHeadersAndBody();
-        parseRequestLineAndHeaders();
-        parseRequestLine();
+    void setRawRequest(String rawRequest) {
+        this.rawRequest = rawRequest;
     }
 
-    void parseRequestLinePlusHeadersAndBody() {
-        String[] splittedRawRequest = rawRequest.split(DOUBLE_CRLF);
-        if(isRawRequestEmpty(splittedRawRequest)) {
-            requestLineAndHeaders = "";
-            requestBody = "";
-        } else if(hasOnlyStringBefore2xCRLF(splittedRawRequest)) {
-            requestLineAndHeaders = rawRequest.split(DOUBLE_CRLF)[0];
-            requestBody = "";
-        } else if(hasStringBeforeAndAfter2xCRLF(splittedRawRequest)) {
-            requestLineAndHeaders = rawRequest.split(DOUBLE_CRLF)[0];
-            requestBody = rawRequest.split(DOUBLE_CRLF)[1];
-        }
+    void createParser() {
+        parser = new RequestParser(rawRequest);
     }
 
-    void parseRequestLineAndHeaders() {
-        String[] splittedRequestLineAndHeaders = requestLineAndHeaders.split(CRLF);
-        requestLine = splittedRequestLineAndHeaders[0];
-        for(int i=1; i < splittedRequestLineAndHeaders.length; i++) {
-            requestHeaders.add(splittedRequestLineAndHeaders[i]);
-        }
+    private String body() {
+        return parser.getRequestBody();
     }
 
-    void parseRequestLine() {
-        String[] splittedRequestLine = requestLine.split(" ");
-        requestMethod = splittedRequestLine[0];
-        requestedResource = splittedRequestLine[1];
-        protocol = splittedRequestLine[2];
+    private ArrayList<String> headers() {
+        return parser.getRequestHeaders();
     }
 
-    private boolean hasStringBeforeAndAfter2xCRLF(String[] stringArray) {
-        return stringArray.length > 1;
+    private String protocol() {
+        return parser.getRequestProtocol();
     }
 
-    private boolean hasOnlyStringBefore2xCRLF(String[] stringArray) {
-        return stringArray.length == 1;
+    private String resource() {
+        return parser.getRequestedResource();
     }
 
-    private boolean isRawRequestEmpty(String[] stringArray) {
-        return rawRequest.equals("") || stringArray.length == 0;
-    }
-
-    String getRequestLineAndHeaders() {
-        return requestLineAndHeaders;
-    }
-
-    ArrayList<String> getRequestHeaders() {return requestHeaders;}
-
-    String getRequestLine() {
-        return requestLine;
-    }
-
-    String getRequestBody() {
-        return requestBody;
-    }
-
-    String getRequestMethod() {
-        return requestMethod;
-    }
-
-    String getRequestedResource() {
-        return requestedResource;
-    }
-
-    String getRequestProtocol() {
-        return protocol;
+    private String method() {
+        return parser.getRequestMethod();
     }
 }
