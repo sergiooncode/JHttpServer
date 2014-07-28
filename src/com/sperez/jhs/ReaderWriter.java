9f9ed79bb05d7ad737f9af153a7bf3a9e9cd8ebf
Sergio Perez <sperez4mba@gmail.com>
2014-07-28 09:04:41 -0500

    Added field to Response for binary body. Changed ReaderWriter to handle the field and refactored it.

--- src/com/sperez/jhs/ReaderWriter.java
+++ src/com/sperez/jhs/ReaderWriter.java
@@ -2,14 +2,15 @@ package com.sperez.jhs;
 
 import java.io.*;
 import java.io.IOException;
-import java.net.*;
 
 public class ReaderWriter {
+    final private String CRLF = "\r\n";
     private InputStreamReader in;
     private BufferedReader inFromClient;
     private BufferedOutputStream out;
     private OutputStreamWriter outToClient;
     private SocketInterface connectionSocket;
+    private Response responseObject;
 
     public ReaderWriter(ConnectionHandler connectionHandler) {
         connectionSocket = connectionHandler.getConnectionSocket();
@@ -19,32 +20,37 @@ public class ReaderWriter {
 
         out = new BufferedOutputStream (connectionSocket.getOutput());
         outToClient = new OutputStreamWriter(out);
-//        outToClient = new OutputStreamWriter(new BufferedOutputStream (connectionSocket.getOutput()));
     }
 
-    public void writeLine(String line) {
-        try{
-            outToClient.write(line);
-        } catch(IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    public void writeAll(String response){
-        try{
-            String[] tokenizedResponse = response.split("\r\n");
-            for(int i=0; i < tokenizedResponse.length; i++){
-                outToClient.write(tokenizedResponse[i]);
-            }
-        } catch(IOException e){
-            e.printStackTrace();
-        }
+    public void writeLine(Response responseObject) {
+        this.responseObject = responseObject;
+        writeStatusLine();
+        writeHeaders();
+        writeBody();
     }
 
     public void sendAll(){
         try{
             outToClient.flush();
         } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void closeReader() {
+        try {
+            inFromClient.close();
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeWriter() {
+        try {
+            outToClient.close();
+            out.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -58,5 +64,36 @@ public class ReaderWriter {
             e.printStackTrace();
         }
         return lineReceived;
+    }
+
+    private void writeStatusLine() {
+        try {
+            outToClient.write(responseObject.getStatusLine() + CRLF);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeHeaders() {
+        try {
+            for (int i = 0; i < responseObject.getResponseHeaders().size(); i++) {
+                outToClient.write(responseObject.getResponseHeaders().get(i) + CRLF);
+            }
+            outToClient.write(CRLF);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void writeBody() {
+        try {
+            if (responseObject.getBinaryResponseBody() != null) {
+                out.write(responseObject.getBinaryResponseBody());
+            } else {
+                outToClient.write(responseObject.getResponseBody());
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- src/com/sperez/jhs/Response.java
+++ src/com/sperez/jhs/Response.java
@@ -6,11 +6,13 @@ public class Response {
     private String statusLine;
     private ArrayList<String> responseHeaders;
     private String responseBody;
+    private byte[] binaryResponseBody;
 
-    public Response(String statusLine, ArrayList<String> responseHeaders, String responseBody) {
+    public Response(String statusLine, ArrayList<String> responseHeaders, String responseBody, byte[] binaryResponseBody) {
         this.statusLine = statusLine;
         this.responseHeaders = responseHeaders;
         this.responseBody = responseBody;
+        this.binaryResponseBody = binaryResponseBody;
     }
 
     public String getStatusLine() {
@@ -23,5 +25,9 @@ public class Response {
 
     public String getResponseBody() {
         return responseBody;
+    }
+
+    public byte[] getBinaryResponseBody() {
+        return binaryResponseBody;
     }
 }
