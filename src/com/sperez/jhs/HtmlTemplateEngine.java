3dc754286d4099339c2939034398cf4cf9b6f048
Sergio Perez <sperez4mba@gmail.com>
2014-07-28 11:41:47 -0500

    Created an static class for html templating.

---
+++ src/com/sperez/jhs/HtmlTemplateEngine.java
@@ -0,0 +1,23 @@
+package com.sperez.jhs;
+
+class HtmlTemplateEngine {
+    static String pageStringWithALinks(String result, int port) {
+        String[] splittedResult = result.split("\n");
+        String templatedResult = "";
+        for(int i=0; i < splittedResult.length; i++) {
+            templatedResult = templatedResult + "<a href=\"http://localhost:" + Integer.toString(port) + "/" +
+            splittedResult[i] + "\">" + splittedResult[i] + "</a></br>";
+        }
+        return templatedResult;
+    }
+
+    static String page404() {
+        String FourOhFour = "<html><body><h1>404 Not Found</h1></body></html>";
+        return FourOhFour;
+    }
+
+    static String page405() {
+        String FourOhFive = "<html><body><h1>405 Method Not Allowed</h1></body></html>";
+        return FourOhFive;
+    }
+}
