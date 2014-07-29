87e6444bbf5736f53c81c07b6abf54a225a708e8
Sergio Perez <sperez4mba@gmail.com>
2014-07-29 16:59:43 -0500

    Added setters methods to Response class.

--- src/com/sperez/jhs/Response.java
+++ src/com/sperez/jhs/Response.java
@@ -18,16 +18,28 @@ public class Response {
     public String getStatusLine() {
         return statusLine;
     }
+    public void setStatusLine(String statusLine) {
+        this.statusLine = statusLine;
+    }
 
     public ArrayList<String> getResponseHeaders() {
         return responseHeaders;
+    }
+    public void setResponseHeader(String header) {
+        responseHeaders.add(header);
     }
 
     public String getResponseBody() {
         return responseBody;
     }
+    public void setResponseBody(String body) {
+        this.responseBody = body;
+    }
 
     public byte[] getBinaryResponseBody() {
         return binaryResponseBody;
+    }
+    public void setBinaryResponseBody(byte[] binaryBody) {
+        this.binaryResponseBody = binaryBody;
     }
 }
