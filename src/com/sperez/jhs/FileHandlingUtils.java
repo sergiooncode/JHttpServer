6c667299392c099018af6b425c68290519957581
Sergio Perez <sperez4mba@gmail.com>
2014-07-29 16:30:53 -0500

    Added more methods.

--- src/com/sperez/jhs/FileHandlingUtils.java
+++ src/com/sperez/jhs/FileHandlingUtils.java
@@ -1,13 +1,16 @@
 package com.sperez.jhs;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Base64;
 
 class FileHandlingUtils {
-    static String lengthOfByteArray(String path) {
+    final static Charset DEFAULT_ENCODING = Charset.defaultCharset();
+
+    static String lengthOfFileInBytes(String path) {
         int length = 0;
         try {
             length = Files.readAllBytes(Paths.get(path)).length;
@@ -17,25 +20,67 @@ class FileHandlingUtils {
         return Integer.toString(length);
     }
 
-    static String readTextFile(String path, Charset encoding) {
+    static String readTextFile(String path) {
         byte[] encoded = new byte[0];
         try {
             encoded = Files.readAllBytes(Paths.get(path));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new String(encoded, encoding);
+        return new String(encoded, DEFAULT_ENCODING);
     }
 
-    static String readImageFile(String path) {
-        byte[] sourceFileInBytes;
-        String encoded = "";
+    static byte[] readImageFile(String path) {
+        byte[] sourceFileInBytes = null;
         try {
             sourceFileInBytes = Files.readAllBytes(Paths.get(path));
-            encoded = Base64.getEncoder().encodeToString(sourceFileInBytes);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return encoded;
+        return sourceFileInBytes;
+    }
+
+    static String listFilesInFolder(String path) {
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+        String list = "";
+        for (int i=0; i < listOfFiles.length; i++) {
+            list = list + listOfFiles[i].getName() + "\n";
+        }
+        return list;
+    }
+
+    static boolean isTxtFile(String requestedResource) {
+        return requestedResource.contains("txt");
+    }
+
+    static boolean isImage(String requestedResource) {
+        return isJpeg(requestedResource) || isGif(requestedResource) || isPng(requestedResource);
+    }
+
+    static boolean isJpeg(String requestedResource) {
+        return requestedResource.contains("jpeg");
+    }
+
+    static boolean isGif(String requestedResource) {
+        return requestedResource.contains("gif");
+    }
+
+    static boolean isPng(String requestedResource) {
+        return requestedResource.contains("png");
+    }
+
+    static boolean isFileInPublicAndIsNotDir(String path) {
+        File file = createFileObject(path);
+        return existsAndIsNotDir(file);
+    }
+
+    private static File createFileObject(String path) {
+        File file = new File(path);
+        return file;
+    }
+
+    private static boolean existsAndIsNotDir(File file) {
+        return file.exists() && !file.isDirectory();
     }
 }
